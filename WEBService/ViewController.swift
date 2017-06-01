a36f0eb341457912ac5254767d4b9c892bb2e7e8
labmacmini02 <labmacmini02@Mac-mini-de-labmacmini02.local>
2017-06-01 08:38:54 -0300

    Initial Commit

---
+++ WEBService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WEBService
+//
+//  Created by labmacmini02 on 01/06/17.
+//  Copyright © 2017 Unitins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
